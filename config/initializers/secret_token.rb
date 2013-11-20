8f46a9d090f923d8a572ed5ab923e6810b3fb9c8
ivandimitrov07 <idimitrov07@yahoo.com>
2013-11-20 18:17:10 +0200

    initial commit:vcard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ivandimitrovvcard::Application.config.secret_token = 'ff8d17eb538b2ed9589c324c0d1347bdbe68e94b8d393b028223d946eb00c6e1661d83396fef2018076419200988f93f06f18c81f992e68f4bbec4c89aa6e839'
